3a64c3bcb1ad4cf9c8683a0b13c87a7e455b3d6f
DivyanshKushwaha2023 <divyanshkushwaha2407@gmail.com>
2024-01-22 22:23:45 +0530

    Create a function to get computer's choice
    created getComputerChoice() for computer's choice using Math.random()

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 //Get player's choice and store it in playerSelection variable
 const playerSelection = prompt("Your choice?", "");
 
-
 //Write a function getComputerChoice() to get computer's choice and store it in computerSelection variable
+const randomNum = Math.floor(Math.random() * 3);
 
-//Write a function round() which plays single round of rock-paper-scissors that takes two parameters playerSelection and computerSelection and returns winner of the round with some string
+function getComputerChoice () {
+    if (randomNum === 0){
+        return "ROCK";
+    } else if (randomNum === 1){
+        return "PAPER";
+    } else {
+        return "SCISSORS"
+    }
+};
+
+const computerSelection = getComputerChoice();
+
+console.log(computerSelection);
+
+//Write a function playRound() which plays single round of rock-paper-scissors that takes two parameters playerSelection and computerSelection and returns winner of the round with some string
 
 //Make playerSelection case insensetive and account for ties by replaying more rounds
 
