f563e1ede8f78d905d2e60dfe394533c511d2dad
DivyanshKushwaha2023 <divyanshkushwaha2407@gmail.com>
2024-01-22 21:16:25 +0530

    Create basic html and script files
    created index.html, script.js files and a variable playerSelection to get player's choice

---
+++ script.js
@@ -0,0 +1,11 @@
+//Get player's choice and store it in playerSelection variable
+const playerSelection = prompt("Your choice?", "");
+
+
+//Write a function getComputerChoice() to get computer's choice and store it in computerSelection variable
+
+//Write a function round() which plays single round of rock-paper-scissors that takes two parameters playerSelection and computerSelection and returns winner of the round with some string
+
+//Make playerSelection case insensetive and account for ties by replaying more rounds
+
+//Write a function game() using previous function as its parameter that plays 5 rounds and displays the results with some strigs
