8d187585a0f88f447128ebebac30afa3e83d21af
DivyanshKushwaha2023 <divyanshkushwaha2407@gmail.com>
2024-01-23 04:27:11 +0530

    Create game() that play 5 round and outputs result
    created game() function that plays 5 rounds and gives result in words with score counts

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 //Get player's choice and store it in playerSelection variable
-let playerSelection = prompt("Your choice?", "").toUpperCase();
 
-console.log(playerSelection);
+function getPlayerChoice(){
+    return prompt("Your choice?", "").toUpperCase();
+}
+
+
 //Write a function getComputerChoice() to get computer's choice and store it in computerSelection variable
 
-const randomNum = Math.floor(Math.random() * 3);
+
 
 function getComputerChoice () {
+    const randomNum = Math.floor(Math.random() * 3);
     if (randomNum === 0){
         return "ROCK";
     } else if (randomNum === 1){
@@ -16,25 +20,53 @@ function getComputerChoice () {
     }
 };
 
-let computerSelection = getComputerChoice();
 
-console.log(computerSelection);
+
 
 //Write a function playRound() which plays single round of rock-paper-scissors that takes two parameters playerSelection and computerSelection and returns winner of the round with some string
 
-function playRound(){
+function playRound(playerSelection, computerSelection){
     if (playerSelection === computerSelection){
         return "TIE";
     } else if ((playerSelection === "ROCK" && computerSelection === "SCISSORS")||(playerSelection==="PAPER" && computerSelection==="ROCK")||(playerSelection==="SCISSORS" && computerSelection==="PAPER")){
-        return `You win!! ${playerSelection} beats ${computerSelection}.`
+        return `You won!! ${playerSelection} beats ${computerSelection}.`
     } else if ((playerSelection === "ROCK" && computerSelection === "PAPER")||(playerSelection==="PAPER" && computerSelection==="SCISSORS")||(playerSelection==="SCISSORS" && computerSelection==="ROCK")){
-        return `You lose!! ${computerSelection} beats ${playerSelection}`
+        return `You lost!! ${computerSelection} beats ${playerSelection}`
     } else{
         return "Invalid input."
     }
-
 }
 
-console.log(playRound());
 
-//Write a function game() using previous function as its parameter that plays 5 rounds and displays the results with some strigs
+//Write a function game() using previous function that plays 5 rounds and displays the results with some strigs
+
+function game() {
+    let playerPoint = 0;
+    let computerPoint = 0;
+    for (let i = 0; i < 5; i++) {
+        let playerSelection = getPlayerChoice();
+        let computerSelection = getComputerChoice();     
+        playRound(playerSelection, computerSelection);
+        let roundResult = playRound(playerSelection, computerSelection);
+       
+        console.log(`Computer selected ${computerSelection}`);
+        console.log(playRound(playerSelection, computerSelection));
+       
+        if (roundResult.substring(0,7) ==="You won" ) {
+            playerPoint += 1;
+        } else if (roundResult.substring(0,7) === "You los") {
+            computerPoint += 1;
+        }
+       
+        console.log(`Your Score- ${playerPoint} and Computer's Score- ${computerPoint}`);
+    }
+    if (playerPoint>computerPoint) {
+       return console.log(`You won!! you scored ${playerPoint} Points and computer scored ${computerPoint} Points.`);
+    } else if (playerPoint<computerPoint){
+       return console.log(`You lost!! computer scored ${computerPoint} Points and you scored ${playerPoint} Points.`)
+    } else {
+        return console.log(`Its a Tie you both scored ${playerPoint} Points.`)
+    }
+}
+
+game();
