b8e950eaccfc8cf53acda44c4b9388fe294057e2
DivyanshKushwaha2023 <divyanshkushwaha2407@gmail.com>
2024-01-22 23:32:02 +0530

    Create playRound() that returns result of 1 round
    created a function playRound() that returns th result of one round of play

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 //Get player's choice and store it in playerSelection variable
-const playerSelection = prompt("Your choice?", "");
+let playerSelection = prompt("Your choice?", "").toUpperCase();
 
+console.log(playerSelection);
 //Write a function getComputerChoice() to get computer's choice and store it in computerSelection variable
+
 const randomNum = Math.floor(Math.random() * 3);
 
 function getComputerChoice () {
@@ -14,12 +16,25 @@ function getComputerChoice () {
     }
 };
 
-const computerSelection = getComputerChoice();
+let computerSelection = getComputerChoice();
 
 console.log(computerSelection);
 
 //Write a function playRound() which plays single round of rock-paper-scissors that takes two parameters playerSelection and computerSelection and returns winner of the round with some string
 
-//Make playerSelection case insensetive and account for ties by replaying more rounds
+function playRound(){
+    if (playerSelection === computerSelection){
+        return "TIE";
+    } else if ((playerSelection === "ROCK" && computerSelection === "SCISSORS")||(playerSelection==="PAPER" && computerSelection==="ROCK")||(playerSelection==="SCISSORS" && computerSelection==="PAPER")){
+        return `You win!! ${playerSelection} beats ${computerSelection}.`
+    } else if ((playerSelection === "ROCK" && computerSelection === "PAPER")||(playerSelection==="PAPER" && computerSelection==="SCISSORS")||(playerSelection==="SCISSORS" && computerSelection==="ROCK")){
+        return `You lose!! ${computerSelection} beats ${playerSelection}`
+    } else{
+        return "Invalid input."
+    }
+
+}
+
+console.log(playRound());
 
 //Write a function game() using previous function as its parameter that plays 5 rounds and displays the results with some strigs
